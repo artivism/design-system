de5632abec0fa8ac9d196f6357cf18b71d729322
chryzeitgeist <44706859+chryzeitgeist@users.noreply.github.com>
2026-02-20 13:34:40 +0100

    chore: migrate apps/design to cloudflare workers

--- apps/design/next.config.js
+++ apps/design/next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   transpilePackages: ['@artivism/tokens', '@artivism/ui'],
-  output: 'export',
   images: {
     unoptimized: true
   }
